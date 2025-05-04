def667ed4e4c4e3140d5f96983b09dbbe557d747
emustur <157262191+emustur@users.noreply.github.com>
2025-05-04 16:52:27 -0500

    Completed Version of Game

---
+++ ReactionProject.py
@@ -0,0 +1,27 @@
+from gpiozero import LED, Button
+from time import sleep
+from random import uniform
+from sys import exit
+
+led = LED(4)
+right_button = Button(15)
+left_button = Button(14)
+
+left_name = input('left player name is ')
+right_name = input('right player name is ')
+
+led.on()
+sleep(uniform(5, 10))
+led.off()
+
+
+def pressed(button):
+	if button.pin.number == 14:
+		print(left_name + ' won the game')
+	else:
+		print(right_name + ' won the game')
+	exit()
+
+
+right_button.when_pressed = pressed
+left_button.when_pressed = pressed
